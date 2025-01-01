ba429aa1bf33f3faf3660b224fdc05d5d322bf03
ayush kumar <ayush2422005@gmail.com>
2025-01-01 19:13:33 +0530

    updates

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Add any necessary Next.js config options
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+export default nextConfig;
